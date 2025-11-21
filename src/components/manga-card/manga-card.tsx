3ea175826507d6de2130b8dec78c0fc796197173
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-21 15:54:44 -0300

    feat: basic test to get a cover image

--- src/components/manga-card/manga-card.tsx
+++ src/components/manga-card/manga-card.tsx
@@ -2,11 +2,11 @@ import { Image, View } from 'react-native';
 
 import { Text } from '../text/text';
 
-export function MangaCard() {
+export function MangaCard({ imageUrl }: { imageUrl: string | null }) {
   return (
     <View className="w-[30%] gap-2">
       <Image
-        source={{ uri: 'https://github.com/fabiano-bragaaa.png' }}
+        source={{ uri: imageUrl ?? 'https://github.com/fabiano-bragaaa.png' }}
         className="aspect-[3/4] w-full rounded-lg"
       />
       <Text size="sm" numberOfLines={2}>
--- src/screens/app/home/home.tsx
+++ src/screens/app/home/home.tsx
@@ -1,18 +1,50 @@
 import { MangaCard, Page } from '@components';
 import { type AppTabScreenProps } from '@routes';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 export function HomeScreen({}: AppTabScreenProps<'Home'>) {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const url = 'https://api.mangadex.org/manga?limit=2&includes[]=cover_art';
+
+  async function testCover() {
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+
+      const manga = json.data[0];
+      const mangaId = manga.id;
+
+      const cover = manga.relationships.find(
+        (r: any) => r.type === 'cover_art'
+      );
+
+      if (!cover) {
+        console.log('Nenhuma capa encontrada.');
+        return;
+      }
+
+      const fileName = cover.attributes.fileName;
+
+      const imageUrl = `https://uploads.mangadex.org/covers/${mangaId}/${fileName}`;
+
+      console.log('IMAGE URL => ', imageUrl);
+      setImageUrl(imageUrl);
+    } catch (err) {
+      console.log('Erro:', err);
+    }
+  }
+
+  useEffect(() => {
+    testCover();
+  }, []);
+
   return (
     <Page>
-      <View className="flex-row flex-wrap gap-4">
-        <MangaCard />
-        <MangaCard />
-        <MangaCard />
-        <MangaCard />
-        <MangaCard />
-        <MangaCard />
+      <View className="flex-row flex-wrap items-center gap-4">
+        <MangaCard imageUrl={imageUrl} />
+        <MangaCard imageUrl={imageUrl} />
+        <MangaCard imageUrl={imageUrl} />
       </View>
     </Page>
   );
