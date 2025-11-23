4a2ea1cd30c6c2778ef5ef48112f47f05ef7ca78
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-22 21:48:57 -0300

    feat: use pagination in home

--- src/infra/operations/index.ts
+++ src/infra/operations/index.ts
@@ -1,2 +1,4 @@
+export * from './types';
 export * from './use-app-mutation';
 export * from './use-app-query';
+export * from './use-paginated-list';
--- src/screens/app/home/home.tsx
+++ src/screens/app/home/home.tsx
@@ -1,13 +1,13 @@
 import { MangaCard, Page } from '@components';
 import { type AppTabScreenProps } from '@routes';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, FlatList, View } from 'react-native';
 import { useGetMangaList } from 'src/domain/manga/use-cases/use-get-manga-list';
 
 export function HomeScreen(_props: AppTabScreenProps<'Home'>) {
-  const { data, isLoading } = useGetMangaList();
+  const { list, isLoading, fetchNextPage, hasNextPage } = useGetMangaList();
 
-  if (isLoading || !data) {
+  if (isLoading || !list) {
     return (
       <View className="flex-1 items-center justify-center">
         <ActivityIndicator size="large" color="#0000ff" />
@@ -17,15 +17,26 @@ export function HomeScreen(_props: AppTabScreenProps<'Home'>) {
 
   return (
     <Page>
-      <View className="flex-row flex-wrap items-center gap-4">
-        {data.map((manga) => (
-          <MangaCard
-            key={manga.id}
-            imageUrl={manga.imageUrl}
-            title={manga.title.en}
-          />
-        ))}
-      </View>
+      <FlatList
+        data={list}
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+          marginBottom: 16,
+        }}
+        contentContainerStyle={{
+          padding: 16,
+          gap: 16,
+        }}
+        renderItem={({ item }) => (
+          <MangaCard imageUrl={item.imageUrl} title={item.title.en} />
+        )}
+        onEndReached={() => {
+          if (hasNextPage) fetchNextPage();
+        }}
+        onEndReachedThreshold={0.3}
+      />
     </Page>
   );
 }
