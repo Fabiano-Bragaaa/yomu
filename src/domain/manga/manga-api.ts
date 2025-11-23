6217379057caf978cc32a00ca03db48de52e9560
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-22 21:48:19 -0300

    feat: change api and service to use paginted response

--- src/domain/manga/manga-api.ts
+++ src/domain/manga/manga-api.ts
@@ -2,11 +2,13 @@ import { api } from '@api';
 
 import { type MangaDexResponse } from './manga-type';
 
-async function getManga(): Promise<MangaDexResponse> {
+async function getManga(offset = 0, limit = 20): Promise<MangaDexResponse> {
   const { data } = await api.get<MangaDexResponse>('/manga', {
     params: {
       originalLanguage: ['en'],
       includes: ['cover_art'],
+      limit,
+      offset,
     },
     paramsSerializer: {
       indexes: false,
--- src/domain/manga/manga-service.ts
+++ src/domain/manga/manga-service.ts
@@ -1,10 +1,17 @@
+import { type PaginatedResponse } from '@infra';
+
 import { mangaAdapter } from './manga-adapter';
 import { mangaApi } from './manga-api';
 import { type MangaSimple } from './manga-type';
 
-async function getManga(): Promise<MangaSimple[]> {
+async function getManga(): Promise<PaginatedResponse<MangaSimple>> {
   const response = await mangaApi.getManga();
-  return mangaAdapter.toMangaSimpleList(response);
+  return {
+    data: mangaAdapter.toMangaSimpleList(response),
+    limit: response.limit,
+    offset: response.offset,
+    total: response.total,
+  };
 }
 export const mangaService = {
   getManga,
