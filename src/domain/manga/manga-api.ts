19e6a4f90c65a55c8db9afe584f303453eba1e79
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-24 00:28:55 -0300

    feat: create initial domain config to get manga by id

--- src/domain/manga/manga-api.ts
+++ src/domain/manga/manga-api.ts
@@ -18,6 +18,20 @@ async function getManga(offset = 0, limit = 20): Promise<MangaDexResponse> {
   return data;
 }
 
+async function getMangaById(id: string): Promise<MangaDexResponse> {
+  const { data } = await api.get<MangaDexResponse>(`/manga/${id}`, {
+    params: {
+      originalLanguage: ['en'],
+      includes: ['cover_art'],
+    },
+    paramsSerializer: {
+      indexes: false,
+    },
+  });
+  return data;
+}
+
 export const mangaApi = {
   getManga,
+  getMangaById,
 };
--- src/domain/manga/manga-service.ts
+++ src/domain/manga/manga-service.ts
@@ -2,7 +2,7 @@ import { type PaginatedResponse } from '@infra';
 
 import { mangaAdapter } from './manga-adapter';
 import { mangaApi } from './manga-api';
-import { type MangaSimple } from './manga-type';
+import { type MangaDexResponse, type MangaSimple } from './manga-type';
 
 async function getManga(offset = 0): Promise<PaginatedResponse<MangaSimple>> {
   const response = await mangaApi.getManga(offset);
@@ -13,6 +13,13 @@ async function getManga(offset = 0): Promise<PaginatedResponse<MangaSimple>> {
     total: response.total,
   };
 }
+
+async function getMangaById(id: string): Promise<MangaDexResponse> {
+  const response = await mangaApi.getMangaById(id);
+  return response;
+}
+
 export const mangaService = {
   getManga,
+  getMangaById,
 };
