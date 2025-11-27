24c49dcb706143d783047b596e3f8227b636f3ed
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-27 11:29:39 -0300

    fix: delete use get chapters by manga

--- src/domain/manga/use-cases/index.ts
+++ src/domain/manga/use-cases/index.ts
@@ -1,3 +1,2 @@
 export * from './use-get-chapter-pages';
-export * from './use-get-chapters-by-manga-id';
 export * from './use-get-manga-by-id';
--- src/domain/manga/use-cases/use-get-chapters-by-manga-id.ts
+++
@@ -1,9 +0,0 @@
-import { queryKeys, usePaginatedList } from '@infra';
-
-import { mangaService } from '../manga-service';
-
-export function useGetChaptersByMangaId(mangaId: string) {
-  return usePaginatedList(queryKeys.chaptersByMangaId(mangaId), (offset) =>
-    mangaService.getChaptersByMangaId(mangaId, offset)
-  );
-}
