233dc65bd946bde2a915daef33930c0f869d3788
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-02 22:00:09 -0300

    fix: add loading when the image not was loaded

--- src/screens/app/chapter/components/chapter-item.tsx
+++ src/screens/app/chapter/components/chapter-item.tsx
@@ -1,6 +1,13 @@
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { Dimensions, Image, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 type ChapterItemProps = {
   uri: string;
@@ -9,21 +16,34 @@ type ChapterItemProps = {
 export function ChapterItem({ uri }: ChapterItemProps) {
   const navigation = useNavigation();
   const { width, height } = Dimensions.get('window');
+  const [loading, setLoading] = useState(true);
   return (
-    <View style={{ width, height }}>
+    <View
+      style={{ width, height }}
+      >
       <TouchableOpacity
         className="absolute z-10 self-start p-4"
         onPress={navigation.goBack}
       >
         <Feather name="arrow-left" size={24} color="white" />
       </TouchableOpacity>
+     
+
       <Image
         source={{
           uri,
         }}
         style={{ width, height }}
         resizeMode="contain"
+        onLoadStart={() => setLoading(true)}
+        onLoadEnd={() => setLoading(false)}
+        onError={() => setLoading(false)}
       />
+       {loading && (
+          <View className='absolute w-full h-full z-10 justify-center items-center'>
+            <ActivityIndicator size="large" color="#fff" />
+          </View>
+        )}
     </View>
   );
 }
