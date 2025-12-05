ab54215d699370c1e282ddb77387cf76df35e265
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-04 22:25:28 -0300

    feat: create manga skeleton component

---
+++ src/components/skeletons/manga-skeleton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ContentLoader, { Rect } from 'react-content-loader/native';
+
+import { type SkeletonSizeProps } from './types';
+
+export function MangaSkeleton({ height, width }: SkeletonSizeProps) {
+  return (
+    <ContentLoader
+      viewBox={`0 0 ${width} ${height}`}
+      width={width}
+      height={height}
+      backgroundColor="#2a2a2a"
+      foregroundColor="#3a3a3a"
+    >
+      <Rect x="0" y="0" rx="12" ry="12" width={width} height={height} />
+    </ContentLoader>
+  );
+}
