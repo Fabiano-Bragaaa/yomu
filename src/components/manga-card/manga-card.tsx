0f09041a6c2e35aa4735835a5480dee9af023f7c
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-04 22:26:03 -0300

    feat: use skeleton when image is loading

--- src/components/manga-card/manga-card.tsx
+++ src/components/manga-card/manga-card.tsx
@@ -1,22 +1,35 @@
-import { Image, Pressable } from 'react-native';
+import { useAppGridSize } from '@hooks';
+import React, { useState } from 'react';
+import { Image, Pressable, View } from 'react-native';
 
+import { MangaSkeleton } from '../skeletons/manga-skeleton';
 import { Text } from '../text/text';
 
 type MangaCardProps = {
   imageUrl: string | null;
   title?: string;
   onPress: () => void;
-  width: number;
 };
 
-export function MangaCard({ imageUrl, title, onPress, width }: MangaCardProps) {
+export function MangaCard({ imageUrl, title, onPress }: MangaCardProps) {
+  const [loading, setLoading] = useState(true);
+  const { ITEM_WIDTH } = useAppGridSize();
+  const IMAGE_HEIGHT = ITEM_WIDTH * (4 / 3);
+
   return (
     <Pressable className="w-[30%] gap-2" onPress={onPress}>
-      <Image
-        source={{ uri: imageUrl ?? undefined }}
-        className="aspect-[3/4] rounded-lg"
-        style={{ width }}
-      />
+      <View className="aspect-[3/4] items-center  justify-center rounded-lg">
+        <Image
+          source={{ uri: imageUrl ?? undefined }}
+          className="absolute inset-0 rounded-lg"
+          style={{ width: '100%', height: '100%' }}
+          onLoad={() => setLoading(false)}
+          onError={() => setLoading(false)}
+        />
+
+        {loading && <MangaSkeleton width={ITEM_WIDTH} height={IMAGE_HEIGHT} />}
+      </View>
+
       <Text size="sm" numberOfLines={2}>
         {title}
       </Text>
