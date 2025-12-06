4b6eeb78da8584f39e21a40184c12fa5938f816e
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-05 21:15:35 -0300

    feat: create favorite button in manga screen

--- src/screens/app/manga/components/manga-header.tsx
+++ src/screens/app/manga/components/manga-header.tsx
@@ -2,7 +2,8 @@ import { Text } from '@components';
 import { type MangaSimple } from '@domain';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { Image, View } from 'react-native';
+import React from 'react';
+import { Image, ImageBackground,  TouchableOpacity, View } from 'react-native';
 
 type MangaHeaderProps = {
   manga: MangaSimple;
@@ -10,6 +11,10 @@ type MangaHeaderProps = {
 
 export function MangaHeader({ manga }: MangaHeaderProps) {
   const navigation = useNavigation();
+
+  function toggleFavorite() {
+    //TODO: toggle favorite     
+  }
 
   return (
     <View className="gap-6">
@@ -19,11 +24,15 @@ export function MangaHeader({ manga }: MangaHeaderProps) {
         color="white"
         onPress={navigation.goBack}
       />
-      <Image
+      <ImageBackground
         source={{ uri: manga.imageUrl ?? undefined }}
         className="aspect-[3/4] w-full rounded-lg"
         resizeMode="cover"
-      />
+      >
+        <TouchableOpacity onPress={toggleFavorite} className='p-4 bg-white rounded-full items-center justify-center self-end m-4'>
+          <Feather name='heart' size={24} color='black' />
+        </TouchableOpacity>
+      </ImageBackground>
       <Text weight="semibold" size={'2xl'}>
         {manga.title.en}
       </Text>
