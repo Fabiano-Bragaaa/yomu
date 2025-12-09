9e7d4b372b170c7163596bd74b035aeb468fa0a8
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-08 22:00:28 -0300

    feat: create manga details skeleton

--- src/components/skeletons/index.ts
+++ src/components/skeletons/index.ts
@@ -1,3 +1,4 @@
 export * from './feed-skeleton';
 export * from './grid-skeleton';
 export * from './rectagle-skeleton';
+export * from './manga-details-skeleton';
--- src/components/skeletons/rectagle-skeleton.tsx
+++ src/components/skeletons/rectagle-skeleton.tsx
@@ -8,6 +8,8 @@ export function RectangleSkeleton({ height, width }: SkeletonSizeProps) {
       viewBox={`0 0 ${width} ${height}`}
       width={width}
       height={height}
+      backgroundColor="#2a2a2a"
+      foregroundColor="#3a3a3a"
     >
       <Rect x="0" y="0" rx="12" ry="12" width={width} height={height} />
     </ContentLoader>
