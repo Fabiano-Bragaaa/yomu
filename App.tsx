54e5e982f73740233efd665c47215aee5897f035
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-10 11:56:27 -0300

    feat: add reactotron in app

--- App.tsx
+++ App.tsx
@@ -12,6 +12,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ActivityIndicator, StatusBar, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+if(__DEV__){
+  require('./reactotron-config')
+}
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
