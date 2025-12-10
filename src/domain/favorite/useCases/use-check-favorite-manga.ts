ae03ddd774baa9d0db6436249e8756513cefc380
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-10 16:28:24 -0300

    style: eslint in use check favorite manga

--- src/domain/favorite/useCases/use-check-favorite-manga.ts
+++ src/domain/favorite/useCases/use-check-favorite-manga.ts
@@ -1,17 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
-import { favoriteService } from '../favorite-service';
 import { queryKeys } from '@infra';
+import { useQuery } from '@tanstack/react-query';
 
-export function useCheckFavoriteManga({mangaId, token}:{ mangaId?: string; token?: string }) {
-  const { data, isPending } = useQuery<boolean, Error, { mangaId: string; token: string }>({
+import { favoriteService } from '../favorite-service';
+
+export function useCheckFavoriteManga({
+  mangaId,
+  token,
+}: {
+  mangaId?: string;
+  token?: string;
+}) {
+  const { data, isPending } = useQuery<
+    boolean,
+    Error,
+    { mangaId: string; token: string }
+  >({
     queryKey: queryKeys.checkFavoriteManga(mangaId!),
-    queryFn: () =>
-      favoriteService.checkMangaFavoriteStatus(mangaId!, token!),
+    queryFn: () => favoriteService.checkMangaFavoriteStatus(mangaId!, token!),
     enabled: !!mangaId && !!token,
   });
 
   return {
     loading: isPending,
     isFavorite: data,
-  }
+  };
 }
