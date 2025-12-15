69ef384639235da3dcb1c4400252686ed64b3bff
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-15 12:09:52 -0300

    feat: setup to render screen to integration test

---
+++ __mocks__/@expo/vector-icons.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const MockIcon = (props: any) => {
+  return React.createElement('Icon', props, null);
+};
+
+export const Feather = MockIcon;
+export const AntDesign = MockIcon;
+export const Ionicons = MockIcon;
+export const MaterialIcons = MockIcon;
+export const MaterialCommunityIcons = MockIcon;
+export const FontAwesome = MockIcon;
+export const FontAwesome5 = MockIcon;
+export const Entypo = MockIcon;
+
+export default {
+  Feather,
+  AntDesign,
+  Ionicons,
+  MaterialIcons,
+  MaterialCommunityIcons,
+  FontAwesome,
+  FontAwesome5,
+  Entypo,
+};
--- jest-setup.ts
+++ jest-setup.ts
@@ -60,6 +60,25 @@ beforeAll(() => {
   };
 });
 
+jest.mock('@react-native-async-storage/async-storage', () => {
+  const mockAsyncStorage = require('@react-native-async-storage/async-storage/jest/async-storage-mock');
+
+  return {
+    __esModule: true,
+    default: mockAsyncStorage,
+  };
+});
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
+
 afterAll(() => {
   console.error = originalError;
 });
